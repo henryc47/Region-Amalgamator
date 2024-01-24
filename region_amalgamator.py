637f087a42871b7969d576278177696856402ae8
Henry Chadban <HenryJ.Chadban@gmail.com>
2024-01-24 15:54:49 +1100

    wrote code to extract unique airport names (combination of name, state, country) to uniquely identify locations, from the airports csv

--- region_amalgamator.py
+++ region_amalgamator.py
@@ -11,6 +11,30 @@ def region_amalgamator(airport_filename,region_filename,output_filename):
     output_filepath = os.path.join(output_folder,output_filename)
     airport_df = pd.read_csv(airport_filepath)
     region_df = pd.read_csv(region_filepath)
+    airport_names,airport_name_indices_dict = get_airport_unique_names(airport_df)
+
+def get_airport_unique_names(df):
+    num_airports = len(df)
+    unique_names = []
+    airport_name_indices_dict = {}
+    for i in range(num_airports):
+        name : str = convert_object_to_str(df.loc[i,"Name"])
+        state : str = convert_object_to_str(df.loc[i,"State"])
+        country : str = convert_object_to_str(df.loc[i,"Country"])
+        unique_name : tuple[str,str,str] = (name,state,country)
+        unique_names.append(unique_name)
+        airport_name_indices_dict[unique_name] = i
+
+    return unique_names,airport_name_indices_dict
+
+#convert pandas object to string
+def convert_object_to_str(object) -> str:
+    if pd.isnull(object):
+         output : str = ""
+    else:
+        output : str = str(object)
+    
+    return output
 
 
 if __name__=="__main__":
